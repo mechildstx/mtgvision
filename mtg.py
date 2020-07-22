a33f37cdccc774128f913a24652fe3de8564be34
Matt Childs <matt@Matts-MacBook-Air.local>
2020-07-21 22:10:09 -0500

    Addition of first file for mtg vision project

---
+++ mtg.py
@@ -0,0 +1,12 @@
+
+from mtgsdk import Card
+from mtgsdk import Set
+from mtgsdk import Type
+from mtgsdk import Supertype
+from mtgsdk import Subtype
+from mtgsdk import Changelog
+
+import requests
+import pandas as pd
+
+card = Card.find(386616)
